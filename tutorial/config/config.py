afe10c98efbe8748cb605960ac4b2b9f5810c312
Evan Bee <evan2884@gmail.com>
2023-12-12 22:49:49 +0800

    Change totorial project

---
+++ tutorial/config/config.py
@@ -0,0 +1,4 @@
+# 生成命令： $ python -c 'import secrets; print(secrets.token_hex())'
+SECRET_KEY = '9806939483073a12ecfe9a6e03baff9cd88b18b9c48c4005e423abbf747f28c0'
+
+SESSION_COOKIE_NAME = 'flaskr.web.apps'
--- tutorial/start.sh
+++ tutorial/start.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/config bash
 set -e
 
+# 第一种启动方式，适用于测试开发阶段
 python -m flask --app flaskr run --host=127.0.0.1 --port=8080 --debugger --reload
+
+# 第二种启动方式，pip包形式启动
+#waitress-serve --call 'flaskr:create_app'
