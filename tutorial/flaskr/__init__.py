2568697511e43f6e09c20dc188053a9a1a81ffd8
Evan Bee <evan2884@gmail.com>
2023-12-12 17:23:08 +0800

    Change totorial project

--- tutorial/flaskr/__init__.py
+++ tutorial/flaskr/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask
-from . import db
+from . import db, auth
 
 
 def create_app(test_config=None):
@@ -32,5 +32,8 @@ def create_app(test_config=None):
 
     # Register database
     db.init_app(app)
+
+    # 导入并注册蓝图
+    app.register_blueprint(auth.bp)
 
     return app
