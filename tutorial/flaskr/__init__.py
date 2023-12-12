a71e0ba97eb74c468f42b70c5e5e3a4e1997274d
Evan Bee <evan2884@gmail.com>
2023-12-12 15:21:30 +0800

    Change totorial project

--- tutorial/flaskr/__init__.py
+++ tutorial/flaskr/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from . import db
 
 
 def create_app(test_config=None):
@@ -29,7 +30,7 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
-    from . import db
+    # Register database
     db.init_app(app)
 
     return app
